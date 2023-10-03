ba11c5a2f3c8030ec958512884ee70e4b8b40f7a
alexander2498 <ebanyjkonc@gmail.com>
2023-10-03 21:24:28 +0530

    feat: complete app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from "react-router-dom";
 import {router} from "./Router/Router";
-// index
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
